e5a83708253e23f62f89504249e367cdcd500af9
kennyzhengkehong <leo.lv@kehong-inno.com>
2026-03-16 11:05:25 +0800

    init

---
+++ TestOpenRouter/TestOpenRouter/TestOpenRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOpenRouterApp.swift
+//  TestOpenRouter
+//
+//  Created by Kehong-IOS-Dev01 on 2026/3/9.
+//
+
+import SwiftUI
+
+@main
+struct TestOpenRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
